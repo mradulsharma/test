901f41c7704103018281d381d2bb3682a40026dc
Maddy <mradul.sharma@gmail.com>
2019-03-13 14:18:59 +1100

    SHA1 encoding a message, and prints hex signature instead of byte

--- EcllipseJava01/src/main/java/com/madiv/crypto/sha1/Sha1HashCreator.java
+++ EcllipseJava01/src/main/java/com/madiv/crypto/sha1/Sha1HashCreator.java
@@ -7,33 +7,23 @@ import java.security.NoSuchAlgorithmException;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.Hex;
-
 public class Sha1HashCreator {
     private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
 	
 	
 	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException, UnsupportedEncodingException {
 		Sha1HashCreator creator = new Sha1HashCreator();
-		String testPayload = "This is my test payload, and its HmacSHA1 has would be generated in this code..";
+		String testPayload = "This is simple message which will be hashed...";
 		String hashKey = "b4ee6662023a2b99537a3e75e6fbfa5f98988836";
 		String signature = creator.generateSignature(testPayload, hashKey);
 		System.out.println("signature for payload is ["+signature+"]");
 	}
 
 	private String generateSignature(String testPayload, String hashKey) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException, UnsupportedEncodingException {
-		String retValue = null;
 		
 		byte[] key;
-        try {
-            key = Hex.decodeHex(hashKey.toCharArray());
-        } catch (DecoderException e) {
-            throw new SecurityException("Invalid HEX key", e);
-        }
 		
-		
+        key = hashKey.getBytes("UTF-8");
         
         // get an hmac_sha1 key from the raw key bytes
         SecretKeySpec signingKey = new SecretKeySpec(key, HMAC_SHA1_ALGORITHM);   
@@ -45,9 +35,12 @@ public class Sha1HashCreator {
         // compute the hmac on input data bytes
         byte[] rawHmac = mac.doFinal(testPayload.getBytes("UTF-8"));
         
-        // base64-encode the hmac
-        retValue = Base64.encodeBase64String(rawHmac);
+        // Each byte is hex value, so generate string having hex formated bytes sequence.
+        StringBuilder builder = new StringBuilder(100);
+        for (byte b : rawHmac) {
+            builder.append(String.format("%02x", b));
+        }
         
-		return retValue;
+		return builder.toString();
 	}
 }
