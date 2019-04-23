6c6bc6a53644cc2525244f9d84c1a3be04163bf3
Maddy <mradul.sharma@gmail.com>
2019-04-24 08:56:20 +1000

    Creating index file

--- EcllipseJava01/src/main/java/com/madiv/Index.java
+++ EcllipseJava01/src/main/java/com/madiv/Index.java
@@ -3,5 +3,22 @@ package com.madiv;
 public class Index {
 	public static void main(String[] args) {
 		System.out.println("01. Some code helped in development in Bravura Solutions	:	com.madiv.brv");
+		System.out.println("02. Dynamic property reader, load property value in run time : com.madiv.dynamicprops");
+		System.out.println("03. Some regex example including greedy regex :	com.madiv.regex");
+		System.out.println("04. Java 8 Stream and Filters : com.madiv.learning");
+		System.out.println("05. Encrypting Decrypting password using BouncyCastle API : com.madiv.pwd");
+		System.out.println("06. Java JMX beans : com.madiv.jmx");
+		System.out.println("07. AWS SQS client sending and receiving SQS message : com.madiv.sqs");
+		System.out.println("08. Jackson API to read and write JSON objects : com.madiv.jackson");
+		System.out.println("09. JAVA Generic as box : com.madiv.generic.box => SenderClient.java");
+		System.out.println("    Class Diagram : ClassDiagram.ucls");
+		System.out.println("10. Spring basic annotation based : com.madiv.spring");
+		System.out.println("11. Spring AOP annotation based : com.madiv.spring.aop");
+		System.out.println("12. Free Marker template : com.madiv.freemarker");
+		System.out.println("13. java Base64 encoding and decoding messages : com.madiv.crypto.decode => EncoderDecoderClient.java");
+		System.out.println("14. Public Private key generator (propbably not completed) : com.madiv.crypto.keypair => PublicPrivateKeyClient.java");
+		System.out.println("15. HmacSHA1 using hashkey : com.madiv.crypto.sha1 => Sha1HashCreator.java");
+		System.out.println("16. Understanding StringBuilder and writing meta program to process 3.1 million record to write shell script: com.madiv.strbuilder");
+		
 	}
 }
